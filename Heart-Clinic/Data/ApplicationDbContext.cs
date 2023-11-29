fde4765e2538d784145615fa327690af09daffc0
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-11-29 10:05:11 +0100

    Init App

---
+++ Heart-Clinic/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Heart_Clinic.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Heart-Clinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Heart_Clinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
